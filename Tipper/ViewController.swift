c26631ce669071e5d309be8fb7a2828f6ac5cb68
Christopher Euabnk <christopher.eubank785@gmail.com>
2020-01-23 21:17:35 -0800

    Added Gif and updated main program

--- Tipper/ViewController.swift
+++ Tipper/ViewController.swift
@@ -10,11 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var billLabel: UITextField!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
 
+    @IBAction func onTap(_ sender: Any) {
+        view.endEditing(true)
+    }
+    
+    
+    @IBAction func CalculateTip(_ sender: Any) {
+        //Get bill amount
+        let bill = Double(billLabel.text!) ?? 0
+        
+        //Calculate the tip and total
+        let tipPercentage = [0.15,0.20,0.25]
+        let tip = bill * tipPercentage[tipControl.selectedSegmentIndex]
+        let total = bill + tip
+        
+        //Get total and display it
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+    }
+    
 }
 
